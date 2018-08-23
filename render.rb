b8fe389be172af75c179b3a347b78756c9662391
Alberto Klocker <alberotk@gmail.com>
2018-08-23 17:08:15 +1000

    update

--- render.rb
+++ render.rb
@@ -24,7 +24,7 @@ fileData = IO.read("data.json")
 jsonData = JSON.parse(fileData)
 
 definitelyTesla = /[Tt]esla|[Mm]usk|[Ee]lon/
-fudwords = / desperate | rattle| worried |in trouble| fear| drugs | scare |total fraud|grueling|([Ff]ormer|[Ee]x[- ])[Tt]esla| (doom|doomed) |bankrupt|lose money|losing (faith|confidence)|(growing|raising|raised) concerns|xperts doubt|nalysts warn|scrambling/
+fudwords = / desperate | rattle| worried |in trouble| fear| drugs | scare |total fraud|grueling|([Ff]ormer|[Ee]x[- ])[Tt]esla| (doom|doomed) |bankrupt|cash (problem|crunch)|(show) skepticism|lose money|losing (faith|confidence)|(growing|raising|raised) concerns|xperts doubt|nalysts warn|scrambling/
 negate = /([Hh]ire|[Hh]iring)|brings on|little to fear|memes/
 
 scanDate = DateTime.now.to_date.to_s
