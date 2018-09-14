a6da6046bf29a2c6d3d5f9bc4efe8574b3da8a3c
Alberto Klocker <alberotk@gmail.com>
2018-09-14 16:49:30 +1000

    fixed unannounced api change, update

--- getnews.rb
+++ getnews.rb
@@ -13,28 +13,36 @@ duplicateCheck = Set.new
 definitelyTesla = /[Tt]esla|[Mm]usk|[Ee]lon/
 fudwords = /desperate|rattled|trouble|fear|scare|doom|bankrupt|losing faith|concerns/
 
-scanDate = DateTime.now.to_date.to_s
+scanDate = DateTime.now
 entryDate = scanDate
+newArticles = true
 begin
-  for i in 1..5 do
-    for page in 1..99 do
-      e = n.get_everything(q: "tesla", sortBy: "publishedAt", pageSize: 100, to: scanDate, language: "en", page: page)
-      puts "get everything.."
+  while newArticles do
+    for page in 1..10 do
+      #2018-09-13T11:27:40 .iso8601
+      e = n.get_everything(q: "tesla", sortBy: "publishedAt", pageSize: 100, to: scanDate.strftime('%F'), language: "en", page: page)
+      puts "get everything from #{scanDate.strftime('%F')} - records: #{e.size}"
+
+      if e.size == 0
+        newArticles = false
+        break
+      end
+
       e.each do |entry|
-          sourceAndTitle = "#{entry.name} -  #{entry.title}"
+          sourceAndTitle = "#{entry.publishedAt} #{entry.name} -  #{entry.title}"
           unless duplicateCheck.include?(sourceAndTitle)
             puts sourceAndTitle
             duplicateCheck.add(sourceAndTitle)
             @articles.push({"source" => entry.name,"author" => entry.author , "title" => entry.title, "url" => entry.url, "description" => entry.description, "publishedAt" => entry.publishedAt, "urlToImage" => entry.urlToImage })
-            entryDate = Date.parse(entry.publishedAt).to_s
+            entryDate = DateTime.parse(entry.publishedAt)
           end
       end
     end
   puts "query again for entries newer than #{entryDate}"
-  scanDate = entryDate
+  scanDate = entryDate.to_date
   end
-rescue
-  puts "Exausted our limit for today"
+rescue => exception
+  puts "Exausted our limit for today "
 end
 
 File.open("data.json","w") do |f|
